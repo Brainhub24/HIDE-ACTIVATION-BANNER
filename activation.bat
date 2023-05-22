5d0606d50041633b95afa1d29783105ea1ce977e
Jason Prime <600613.m6@gmail.com>
2023-05-22 22:13:06 +0200

    Update #2
    A few improvements.

--- activation.bat
+++ activation.bat
@@ -1,19 +1,22 @@
-@echo off
 REM The "Activate Windows" banner is typically displayed when Windows is not activated with a valid license key.
 REM Restarting Windows Explorer will temporarily remove the banner, but it may reappear later if the underlying activation issue is not resolved.
 REM If you are encountering the "Activate Windows" banner, I would recommend ensuring that your Windows installation is properly activated with a valid license key.
 
 REM Developer: Jan Gebser
 REM Github: @Brainhub24
-REM Code created on: 21.05.2019
+REM Code created: 21.05.2019
+REM Code updated: 22.05.2023
 
-REM This script terminates the explorer.exe process, restarts it, and then exits.
+@echo off
+rem Turn off the display of commands and output
 
-taskkill /F /IM explorer.exe
-REM Taskkill command is used to forcefully terminate the explorer.exe process.
-
-explorer.exe
-REM Starts the explorer.exe process, which manages the Windows desktop and file explorer.
+taskkill /F /IM explorer.exe && (
+  echo Explorer.exe terminated successfully.
+  explorer.exe
+  rem Restart the "explorer.exe" process
+) || (
+  echo Failed to terminate explorer.exe.
+)
 
 exit
-REM Exits the batch script execution.
+rem Exit the batch script
